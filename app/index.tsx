82c695844be6d054d2e65a8ee15b811ff5ab5183
Gabriela <gabriela.rodrigues.1@academico.ifpb.edu.br>
2025-04-29 11:49:48 -0300

    Borda

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
 import { Text, View, StyleSheet } from "react-native";
 
+const dados = [
+  {
+    id: 1,
+    nome: "Gaby",
+    cargo: "estudante"
+  },
+  {
+    id: 2,
+    nome: "Gabi de novo sim",
+    cargo: "teacher's best"
+  }
+];
+
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={style.container}
     >
-      <Text>Chegando já as fotos</Text>
+      {
+      dados.map( item => (
+        <View style={style.itemLista}> 
+          <Text style={style.textoLista}>{item.nome}</Text>
+        </View>
+      ))
+      }
     </View>
   );
 }
@@ -17,7 +32,18 @@ export default function Index() {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
+  },
+
+  itemLista: {
+    width: '100%',
+    padding: 10,
+    marginBottom: 2,
+    backgroundColor: "blue"
+  },
+
+  textoLista: {
+    fontSize: 20
   }
 })
